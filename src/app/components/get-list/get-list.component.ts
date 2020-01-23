f9c7d2ef80eb2e1c3726bdd17a11a4410e95167f
Ramakrishna Pavuluri <shravanishrav8@gmail.com>
2020-01-23 19:44:32 +1100

    medibank assignment

--- src/app/components/get-list/get-list.component.ts
+++ src/app/components/get-list/get-list.component.ts
@@ -12,38 +12,46 @@ export class GetListComponent implements OnInit {
   catsList: any[] = []
   maleCatsList: any[] = []
   femaleCatsList: any[] = []
+  transGenderCatsList: any
   constructor(private http: Http) { }
 
   ngOnInit() {
-    this.http.get('https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json')
-      .subscribe((response: any) => {
-        this.totalList = response.json()
-        this.petsList = this.totalList.filter(list => list.pets && list.pets.length > 0)
-       
-        this.petsList.map(list => {
-           list.pets.map((pet: any) => {
-            if (pet.type.toLowerCase() === 'cat' && !this.catsList.includes(list)) {
-              if(list.gender.toLowerCase() === 'male') {
-                this.maleCatsList.push(pet.name) // To  get male owner cats list
-              } else {
-                this.femaleCatsList.push(pet.name) // To  get female owner cats list
+    try{
+       this.http.get('https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json')
+        .subscribe((response: any) => {
+          this.totalList = response.json()
+          this.petsList = this.totalList.filter(list => list.pets && list.pets.length > 0)
+        
+          this.petsList.map(list => {
+            list.pets.map((pet: any) => {
+              if (pet.type.toLowerCase() === 'cat') {
+                if(list.gender.toLowerCase() === 'male') {
+                  this.maleCatsList.push(pet.name) // To  get male cats list
+                } else if(list.gender.toLowerCase() === 'female') {
+                  this.femaleCatsList.push(pet.name) // To  get female cats list
+                } else {
+                  this.transGenderCatsList = 'transgender'
+                }
+                this.catsList.push(list) //Get the array of having only cats of both male and female (may be useful for any further requirement)
               }
-              this.catsList.push(list) //Get the array of having only cats of both male and female (may be useful for any further requirement)
-             }
-           })
-        })
-        this.maleCatsList.sort(function(x,y) {
-          if (x.name < y.name) return -1;
-          if (x.name > y.name) return 1;
-          return 0;
-        })
-        this.femaleCatsList.sort(function(x,y) { //Sorting alphabetically
-          if (x < y) return -1;
-          if (x > y) return 1;
-          return 0;
-        })
-       
-      });
+            })
+          })
+          this.maleCatsList.sort(function(cat1,cat2) {
+            if (cat1 < cat2) return -1;
+            if (cat1 > cat2) return 1;
+            return 0;
+          })
+          this.femaleCatsList.sort(function(cat1,cat2) { //Sorting alphabetically
+            if (cat1 < cat2) return -1;
+            if (cat1 > cat2) return 1;
+            return 0;
+          })
+      }, error => {
+        console.log(error)
+      })
+    } catch(error) {
+      console.log(error)
+    }
   }
 
 }
