72c709644dd7462e0182476047269d5b33ed3a97
Ramakrishna Pavuluri <shravanishrav8@gmail.com>
2020-01-23 19:59:30 +1100

    medibank

--- src/app/components/get-list/get-list.component.ts
+++ src/app/components/get-list/get-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Http} from '@angular/http';
+import { Component, OnInit } from '@angular/core'
+import {Http} from '@angular/http'
 
 @Component({
   selector: 'app-get-list',
